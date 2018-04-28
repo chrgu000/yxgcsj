6d3bbefeaccf4e98c28a482e10d73a81713309e4
yjfyeyu <yjfyeyu@gmail.com>
2018-04-28 10:38:55 +0800

    0.39

---
+++ 异星工厂世界/svn_cmd.bat
@@ -0,0 +1,4 @@
+rem "./data/facw/svn.exe" co svn://gitee.com/ipup/yxgcip ./data/facw/sl --username ipuppublic --password 1234abcD
+rem "./data/facw/svn.exe" ci ./data/facw/sl -m "new" --username ipuppublic --password 1234abcD
+"./data/facw/svn.exe" ci ./data/facw/sl -m "new" --username ipuppublic --password 1234abcD
+pause
